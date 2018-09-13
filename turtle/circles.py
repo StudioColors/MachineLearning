f37363fbb037c2f2670048928c2729d541623305
IT-STUDENT <it-student@MEPHI3.LESNOY>
2018-09-13 13:48:47 +0300

    Упражнения с черепашкой

---
+++ turtle/circles.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.shape('turtle')
+a=5
+for i in range(1,20):
+    turtle.circle(a, 180)
+    a+=5         
+
+
+
+
+
+
+
+
+
---
+++ turtle/s.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.penup()
+
+turtle.setpos(-350,-350)
+turtle.pendown()
+
+turtle.shape('turtle')
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
